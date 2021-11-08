c488fda9d17835bb8ddc07cb4aefdd8c1518840a
Benjamin Lyons <bencl1998@gmail.com>
2021-11-08 16:38:59 -0500

    Add plotting file

---
+++ plot_task_benchmark.py
@@ -0,0 +1,41 @@
+import sys
+import matplotlib.pyplot as plt
+import pandas as pd
+import csv
+import numpy as np
+
+filenames = ["sum.csv", "adds.csv", "sequential.csv", "increment.csv"]
+titles = ["Reduction", "Shared", "Sequential", "Parallel"]
+
+def extract_data(filename):
+        with open(filename) as f:
+                reader = csv.reader(f)
+                labels = next(reader)
+                data = []
+                for row in reader:
+                        data.append([float(x) for x in row])
+                data = np.array(data)
+        return data
+
+fig, axs = plt.subplots(2, 2)
+for i in range(len(filenames)):
+        title = titles[i]
+        ax = axs[i//2][i % 2]
+        data = extract_data(filenames[i])
+        output_file = "{}.{}".format(filenames[i].replace('.csv', ''),'png')
+
+        x = [ d[0] for d in data ]
+        y = [ d[1] for d in data ]
+
+        ax.plot(x, y)
+        ax.set_title(title)
+        ax.set_xlabel('machines (4 cores each)')
+        ax.set_ylabel('throughput (tasks/sec)')
+        ax.set_xlim([0, max(x) + 10])
+        ax.set_ylim([0, max(y) + max(y)*.1])
+
+plt.tight_layout()
+plt.show()
+plt.savefig("output.png")
+
+
